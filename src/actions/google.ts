867f55080bcd0f5245814489e1e860d727ab5313
0xSJ <jainsanmati846@gmail.com>
2024-05-10 01:33:05 +0530

    feat: Update Google authentication credentials
    This commit updates the Google authentication credentials in the `getglAuth` function of the `google.ts` file. The previous credentials from the `vistaGoogleApis.json` file have been replaced with environment variables for improved security. The new credentials include the `type`, `project_id`, `private_key_id`, `private_key`, `client_email`, `client_id`, and `universe_domain` properties.
    
    Co-authored-by: [Author Name]

--- src/actions/google.ts
+++ src/actions/google.ts
@@ -1,10 +1,17 @@
 'use server'
 import { google } from 'googleapis'
-import vistaGoogleApis from '@/lib/vista-googleApis.json'
 
 const getglAuth = async () => {
   const glAuth = await google.auth.getClient({
-    credentials: vistaGoogleApis,
+    credentials: {
+      type: process.env.GOOGLE_TYPE,
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      universe_domain: 'googleapis.com',
+    },
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   })
 
